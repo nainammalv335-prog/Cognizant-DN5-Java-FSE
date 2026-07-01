eed74d892f9686ad4cc5206f1c7e582a6c8fb43a
nainammalv335-prog <nainammalv335@gmail.com>
2026-07-01 19:31:20 +0530

    Create Maven Project.java

---
+++ week 2/Spring core and Maven/Exercise3/Maven Project.java
@@ -0,0 +1,15 @@
+
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("   Maven Project Configuration");
+    System.out.println("Project Name : CreatingMavenProject");
+        System.out.println("Group Id     : com.cognizant");
+        System.out.println("Artifact Id  : CreatingMavenProject");
+        System.out.println("Version      : 1.0-SNAPSHOT");
+
+        System.out.println("\nMaven Project Created Successfully!");
+    }
+}
