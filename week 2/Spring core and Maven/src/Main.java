7ccf50063c575d5b681d804aaea61df95813b53e
nainammalv335-prog <nainammalv335@gmail.com>
2026-07-01 14:35:50 +0530

    Add Main class for Spring application context

---
+++ week 2/Spring core and Maven/src/Main.java
@@ -0,0 +1,17 @@
+package com.example.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Student student = context.getBean("student", Student.class);
+
+        student.display();
+    }
+}
