bc15fc5cd75db100197bbd3ca8941cea3fb54d15
nainammalv335-prog <nainammalv335@gmail.com>
2026-07-11 14:35:17 +0530

    Create Account.java

---
+++ week 4/Microservices with Spring Boot3 and Spring cloud/Creating Microservices for account and loan/Account.java
@@ -0,0 +1,41 @@
+package com.example.account.model;
+
+public class Account {
+
+    private int accountId;
+    private String accountHolder;
+    private double balance;
+
+    public Account() {
+    }
+
+    public Account(int accountId, String accountHolder, double balance) {
+        this.accountId = accountId;
+        this.accountHolder = accountHolder;
+        this.balance = balance;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void setAccountHolder(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
